80944a50bd2f60992caf03363b6460cac9827b83
danbeePark <danbee207@gmail.com>
2015-11-06 15:37:33 -0500

    // add a bean 'user'

---
+++ Substa/src/substa/beans/User.java
@@ -0,0 +1,25 @@
+package substa.beans;
+
+/**
+ * @author DnB
+ *
+ */
+public class User {
+
+	private String first;
+	private String last;
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	
+	
+}
