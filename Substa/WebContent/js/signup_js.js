d0e8e4f7bc13679766df5d1099c8183a837a7e7e
danbeePark <danbee207@gmail.com>
2015-11-10 12:16:17 -0500

    add singup servlet add js for singup

---
+++ Substa/WebContent/js/signup_js.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+
+function checkingEmailFormat(email){
+	
+	
+}
+
+function checkingPasswordFormat(pw){
+	
+	
+}
+
+function checkingAddressFormat(address){
+	
+}
+
+function checkingSSN(ssn){
+	
+	/*
+	 * The Social Security number is a nine-digit number in the format "AAA-GG-SSSS". The number is divided into three parts.
+The middle two digits are the Group Number. The Group Numbers range from 01 to 99.
+The last four digits are Serial Numbers. They represent a straight numerical sequence of digits from 0001 to 9999 within the group.
+Some special numbers are never allocated:
+Numbers with all zeros in any digit group (000-##-####, ###-00-####, ###-##-0000).
+Numbers with 666 or 900-999 (Individual Taxpayer Identification Number) in the first digit group.
+SSNs used in advertising have rendered those numbers invalid.
+	 * 
+	 */
+}
+function goToServlet(btn){
+	
+	
+	this.submit();
+}
+
---
+++ Substa/src/substa/servlet/SingUp.java
@@ -0,0 +1,49 @@
+package substa.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SingUp
+ */
+@WebServlet(
+		urlPatterns = { "/SingUp" }, 
+		initParams = { 
+				@WebInitParam(name = "jdbcDriver", value = "com.mysql.jdbc.Driver"), 
+				@WebInitParam(name = "dbUrl", value = "jdbc:mysql://mysql2.cs.stonybrook.edu:3306/danpark"), 
+				@WebInitParam(name = "dbUser", value = "danpark"), 
+				@WebInitParam(name = "dbPass", value = "110142214")
+		})
+public class SingUp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SingUp() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
