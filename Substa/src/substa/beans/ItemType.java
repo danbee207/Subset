6e0173de531762e0eab5d20ca83aa69a4850c8a5
danbeePark <danbee207@gmail.com>
2015-11-23 16:13:42 -0500

    add beans info ; item type

--- Substa/src/substa/beans/ItemType.java
+++ Substa/src/substa/beans/ItemType.java
@@ -1,5 +1,34 @@
 package substa.beans;
 
 public enum ItemType {
-
+	
+	Books,
+	Textbooks,
+	Magazines,
+	EBooks,
+	TV_Video,
+	CellPhones,
+	Desktops,
+	Laptops,
+	Womenclothing,
+	menclothing,
+	kidclothing,
+	kitchen,
+	furniture,
+	appliance,
+	flowerTree,
+	Gardening,
+	cars,
+	motorcycles,
+	automotiveTools,
+	teamSports,
+	leisureSports,
+	waterSports,
+	ActionFigures,
+	BuildingToys,
+	BabyToys,
+	Games,
+	Videos
+	
+	
 }
